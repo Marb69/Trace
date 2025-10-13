5ce8b680609adb63ae9c2bb4420d6e493c7ef694
Jhon <jhonalvertmarbebe0@gmail.com>
2025-10-13 12:17:40 +0800

    hero section

--- Js/Script.js
+++ Js/Script.js
@@ -1,22 +1,58 @@
 
 
-const HamMenu = document.querySelector('.ham-menu');
 
+const HamMenu = document.querySelector('.ham-menu');
 
 HamMenu.addEventListener('click',()=>{
 
-    let barMenu = document.querySelectorAll('.bar'); 
+   const barMenu = document.querySelectorAll('.bar'); 
 
      barMenu.forEach(bar =>{
 
         bar.classList.toggle('open');
      })
 
-     let NavBar = document.querySelector('.navbar');
-     let navBlured = document.querySelector('.nav-bg-blured');
+     const NavBar = document.querySelector('.navbar');
+     const navBlured = document.querySelector('.nav-bg-blured');
 
      navBlured.classList.toggle('open-nav');
      NavBar.classList.toggle('open-nav');
 
     
-})
+});
+
+
+
+const BrandsPath = [
+       
+        'Assets/Images/hack_the_box_logo.svg.svg',
+        'Assets/Images/the_linux_foundation_logo.svg.svg',
+        'Assets/Images/microsoft.svg',
+        'Assets/Images/udemy.svg',
+        'Assets/Images/cisco.svg'
+
+];
+
+renderBrand();
+
+
+function renderBrand(){
+
+
+      BrandsPath.forEach((brand)=>{
+
+      const Carousel = document.querySelector('#carousel');
+
+      const Brand = document.createElement('li');
+      Carousel.appendChild(Brand);
+      const img = document.createElement('img');
+      Brand.appendChild(img);
+
+      img.src = brand;
+
+      })
+   
+     
+
+
+}
